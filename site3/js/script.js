6b40e4e51c5b63bcf058f3518f16d674efb087a6
TheNirnir <nirnirba@gmail.com>
2020-04-12 21:11:33 +0300

    message

--- site3/js/script.js
+++ site3/js/script.js
@@ -1,13 +1,21 @@
 // //DOM manipulation
 // console.log(document.getElementById("title"));
-function sayHello () {
-	var name = document.getElementById("name").value;
-	var message = "<h2>Hello " + name + "!</h2";
-	// document.getElementById("content").textContent = message; =
-	// document.getElementById("content").innerHTML = message; =
-	document.querySelector("#content").innerHTML = message;
-	//doesn't work - document.querySelectorAll("div").innerHTML = message;
-	if (name === "Gael Monfils") {
-		document.querySelector("#title").textContent = "Gael Monfils is the best player in the world!";
+document.addEventListener("DOMContentLoaded",
+	function (event) {
+		function sayHello (event) {
+			this.textContent = "said it!"
+			var name = document.getElementById("name").value;
+			var message = "<h2>Hello " + name + "!</h2";
+			// document.getElementById("content").textContent = message; =
+			// document.getElementById("content").innerHTML = message; =
+			document.querySelector("#content").innerHTML = message;
+			//doesn't work - document.querySelectorAll("div").innerHTML = message;
+			if (name === "Gael Monfils") {
+				document.querySelector("#title").textContent = "Gael Monfils is the best player in the world!";
+			}
+		}
+		// unobstrusive event binding
+		// document.querySelector("button").addEventListener("click", sayHello);
+		document.querySelector("button").onclick = sayHello;
 	}
-}
+);

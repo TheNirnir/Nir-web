6fde1e40cf96bf2ac83a814cbac6de6a658e87e1
TheNirnir <nirnirba@gmail.com>
2020-04-15 20:10:23 +0300

    message

--- LabSite/js/script.js
+++ LabSite/js/script.js
@@ -8,39 +8,39 @@ $(function () { // same as document.addEventListener("DOMContentLoaded"...)
 	});
 });
 
-(function (global) {
+// (function (global) {
 
-	var ot = {};
+// 	var ot = {};
 
-	var homeHtml = "snippets/home-snippet.html";
-	var aboutHtml = "snippets/about-snippet.html";
+// 	var homeHtml = "snippets/home-snippet.html";
+// 	var aboutHtml = "snippets/about-snippet.html";
 
-	var html = homeHtml;
-	// convinience function for inserting innerHtml fot 'select'
-	var insertHtml = function (selector, html) {
-		var targetElem = document.querySelector(selector);
-		targetElem.innerHtml = html;
-	};
+// 	var html = homeHtml;
+// 	// convinience function for inserting innerHtml fot 'select'
+// 	var insertHtml = function (selector, html) {
+// 		var targetElem = document.querySelector(selector);
+// 		targetElem.innerHtml = html;
+// 	};
 
-	// showing loading icon inside element identified by 'selector'
-	var showLoading = function (selector) {
-		var html = "<div class='text-center'>";
-		html += "<img src='images/ajax-loader.gif'></div>";
-		insertHtml (selector, html);
-	};
-	// var AboutLoad = function () {
-	// 	html = aboutHtml;
-	// };
-	// on page load - before images or css
-	document.addEventListener("DOMContentLoaded", function (event) {
-		// on first load, show home view
-		showLoading("#main-content");
-		$ajaxUtils.sendGetRequest(html, function (responseText) {
-			document.querySelector("#main-content").innerHTML = responseText;
-		},
-		false);
-	});
+// 	// showing loading icon inside element identified by 'selector'
+// 	var showLoading = function (selector) {
+// 		var html = "<div class='text-center'>";
+// 		html += "<img src='images/ajax-loader.gif'></div>";
+// 		insertHtml (selector, html);
+// 	};
+// 	// var AboutLoad = function () {
+// 	// 	html = aboutHtml;
+// 	// };
+// 	// on page load - before images or css
+// 	document.addEventListener("DOMContentLoaded", function (event) {
+// 		// on first load, show home view
+// 		showLoading("#main-content");
+// 		$ajaxUtils.sendGetRequest(html, function (responseText) {
+// 			document.querySelector("#main-content").innerHTML = responseText;
+// 		},
+// 		false);
+// 	});
 
-global.$ot = ot;
+// global.$ot = ot;
 
-})(window);
+// })(window);

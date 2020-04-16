bc7255bef00e1eaeaa337d1b48affb5060250580
TheNirnir <nirnirba@gmail.com>
2020-04-17 00:58:51 +0300

    message

--- LabSite/js/script.js
+++ LabSite/js/script.js
@@ -1,13 +1,37 @@
-$(function () { // same as document.addEventListener("DOMContentLoaded"...)
-	// same as document.qeurySelector("#navbarToggle").addEventListener("blur",...
-	$("#navbarToggle").blur(function (event) {
-		var screenWidth = window.innerWidth;
-		if (screenWidth<768) {
-			$("#collapsable-nav").collapse('hide');
-		}
-	});
+// $(function () { // same as document.addEventListener("DOMContentLoaded"...)
+// 	// same as document.qeurySelector("#navbarToggle").addEventListener("blur",...
+// 	$("#navbarToggle").blur(function (event) {
+// 		var screenWidth = window.innerWidth;
+// 		if (screenWidth<768) {
+// 			$("#collapsable-nav").collapse('hide');
+// 		}
+// 	});
+// });
+
+document.addEventListener("DOMContentLoaded", function (event) {
+	var coll = document.getElementsByClassName("collapsible-a");
+	var i;
+
+	for (i = 0; i < coll.length; i++) {
+	  coll[i].addEventListener("click", function() {
+	    this.classList.toggle("active-a");
+	    var content = this.nextElementSibling;
+	    if (content.style.display === "block") {
+	      content.style.display = "none";
+	    } else {
+	      content.style.display = "block";
+	    }
+	  });
+	}
 });
 
+var researchOptionsOpen = function () {
+	document.querySelector("#research-options").textContent = "options";
+};
+
+var researchOptionsClose = function () {
+	document.querySelector("#research-options").textContent = "";
+};
 // (function (global) {
 
 // 	var ot = {};

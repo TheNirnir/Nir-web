91fdb91704508091fecff7a216db66e1746b6fe5
TheNirnir <nirnirba@gmail.com>
2020-05-03 20:14:54 +0300

    message

--- LabSite/js/script.js
+++ LabSite/js/script.js
@@ -902,15 +902,12 @@ function stopPlayPicture () {
 	document.getElementById("gallery-play").style.height = "auto";
 }
 
-$(function() {      
-  //Enable swiping...
+$(function() {
   $("#test").swipe( {
     //Generic swipe handler for all directions
     swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
       $(this).text("You swiped " + direction );  
-    },
-    //Default is 75px, set to 0 for demo so any distance triggers swipe
-     threshold:0
+    }
   });
 });
 

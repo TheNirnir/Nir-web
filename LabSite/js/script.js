d54269ad9c0c7163b488d62b252d3b1a9865f145
TheNirnir <nirnirba@gmail.com>
2020-05-09 19:11:12 +0300

    message

--- LabSite/js/script.js
+++ LabSite/js/script.js
@@ -1458,7 +1458,9 @@ function touchswipeStart () {
 	    //Generic swipe handler for all directions
 	    swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
 	      $(this).text("You swiped " + direction );
-	      nextPicture();
+	      if (direction == "left") {
+	      	nextPicture();
+	      }
 	    }
 	  });
 	  // if (pageName != "Gallery") {

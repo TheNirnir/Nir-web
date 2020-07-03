69e88ee70d9a1353f2cdfd93671313bfda8802ba
TheNirnir <nirnirba@gmail.com>
2020-07-03 21:13:31 +0300

    m

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -4,6 +4,21 @@ var number2Start;
 var result = "";
 var globalAction = "";
 
+document.addEventListener("DOMContentLoaded", function (event) {
+	// var screenWidth = screen.width;
+	// screenWidth += "px";
+
+	var optionsHeight = screen.availHeight - 120;
+	optionsHeight += "px";
+
+	// console.log(document.getElementById("options-container").style.width);
+	// document.getElementById("options-container").style.width = screenWidth;
+	document.getElementById("options-container").style.height = optionsHeight;
+
+	// console.log(screen.width);
+	// console.log(document.getElementById("options-container").style.width);
+});
+
 function numbersChoice (number) {
 	if (document.getElementById("numbers-screen").innerHTML == 0) {
 		document.getElementById("numbers-screen").innerHTML = "";

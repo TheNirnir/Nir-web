eaacd8a33431a90f472cff26a5ff6a65dd581f99
TheNirnir <nirnirba@gmail.com>
2020-07-03 18:45:30 +0300

    m

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -1,4 +1,36 @@
+var number1 = "";
+var number2 = "";
+var number2Start;
+var result = "";
+var globalAction = "";
+
 function numbersChoice (number) {
 	console.log(number);
-	document.getElementById("numbers-screen").innerHTML = number;
+	document.getElementById("numbers-screen").innerHTML += number;
+	if (number2Start == "start") {
+		// console.log("start");
+		number2 += number;
+	}
+}
+
+function actionChoice (action) {
+	globalAction = action;
+	number1 = document.getElementById("numbers-screen").innerHTML;
+	number2Start = "start";
+
+	console.log(action);
+	document.getElementById("numbers-screen").innerHTML += action;
+}
+
+function checkChoice () {
+	console.log(number1 + " " + globalAction+ " " + number2)
+	if (globalAction == "+") {
+		result = parseInt(number1) + parseInt(number2);
+	}
+	if (globalAction == "-") {
+		result = parseInt(number1) - parseInt(number2);
+	}
+
+	console.log(result);
+	document.getElementById("numbers-screen").innerHTML += "<br>= " + result;
 }

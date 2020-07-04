405b32cd893922cb4d316992f2710a1d850479fc
TheNirnir <nirnirba@gmail.com>
2020-07-04 12:44:28 +0300

    m

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -9,16 +9,38 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	// screenWidth += "px";
 
 	// var optionsHeight = screen.availHeight - 120;
-	var optionsHeight = screen.availHeight;
-	optionsHeight += "px";
+
+	// var optionsHeight = screen.availHeight;
+	// optionsHeight += "px";
 
 	// console.log(document.getElementById("options-container").style.width);
 	// document.getElementById("options-container").style.width = screenWidth;
 	// document.getElementById("options-container").style.height = optionsHeight;
-	document.getElementById("The-container").style.height = optionsHeight;
+
+	// document.getElementById("The-container").style.height = optionsHeight;
 
 	// console.log(screen.width);
 	// console.log(document.getElementById("options-container").style.width);
+
+	// var internalOptionHeight = document.getElementsByClassName("internal-option").offsetHeight;
+	// var internalOptionPadding = parseInt(internalOptionHeight);
+	// console.log(internalOptionHeight);
+	// internalOptionPadding = internalOptionPadding/4;
+	// internalOptionPadding += "px";
+	// console.log(internalOptionPadding);
+	// document.getElementsByClassName("internal-option").style.paddingTop = internalOptionPadding;
+
+	var internalOptionHeight = document.getElementById("example").offsetHeight;
+	console.log(internalOptionHeight);
+	var internalOptionPadding = parseInt(internalOptionHeight);
+	internalOptionPadding = internalOptionPadding/4;
+	internalOptionPadding += "px";
+	console.log(internalOptionPadding);
+	for (var i = 0; i < document.getElementsByClassName("internal-option").length; i++) {
+		document.getElementsByClassName("internal-option")[i].style.paddingTop = internalOptionPadding;
+		// document.getElementsByClassName("internal-option")[i].style.paddingBottom = internalOptionPadding;
+	}
+	// document.getElementsByClassName("internal-option")[0].style.paddingTop = internalOptionPadding;
 });
 
 function numbersChoice (number) {

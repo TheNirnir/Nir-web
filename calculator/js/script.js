546e4c5c719a8883773cfd6be59a45cb6844d263
TheNirnir <nirnirba@gmail.com>
2020-08-30 15:33:34 +0300

    m

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -11,6 +11,14 @@ var globalDegOrRad = "rad";
 var globalMode = "";
 var quadraticEquationArray = ["","",""];
 var abc = 0;
+var numbersArray = [];
+var actionsArray = [];
+var numbersOrderArray = [];
+var actionsOrderArray = [];
+var numberNumber = 0;
+var actionNumber = 0;
+var thereWasPower = "";
+// var actionsOrder, numbersOrder;
 
 document.addEventListener("DOMContentLoaded", function (event) {
 	changeMode("Regular"); //Quadratic-equation
@@ -82,7 +90,7 @@ function numbersChoice (number) {
 		if (document.getElementById("numbers-box").innerHTML == "0") {
 			document.getElementById("numbers-box").innerHTML = "";
 		}
-		if (number == "Pi" || number == "e" || globalAction == "^") {
+		if (number == "Pi" || number == "e" || globalAction == "^" || thereWasPower == "yes") {
 			if (number == "Pi") {
 				number = Math.PI;
 				document.getElementById("numbers-box").innerHTML += "&Pi;";
@@ -91,15 +99,24 @@ function numbersChoice (number) {
 				number = Math.E;
 				document.getElementById("numbers-box").innerHTML += "e";
 			}
+			if (thereWasPower == "yes") {
+				document.getElementById("numbers-box2").innerHTML += number;
+			}
 			if (globalAction == "^") {
 				document.getElementById("power-numbers-box").innerHTML += number;
-				// document.getElementById("power-numbers-box").style.border = "none";
+				document.getElementById("power-numbers-box").style.border = "none";
 				document.getElementById("power-numbers-box").style.marginTop = "-5px";
+				thereWasPower = "yes";
 			}
 		}
 		else {
 			document.getElementById("numbers-box").innerHTML += number;
 		}
+		if (numbersArray[numberNumber] == null) {
+			console.log("null");
+			numbersArray[numberNumber] = "";
+		}
+		numbersArray[numberNumber] += number;
 		console.log(number);
 		if (number2Start == "start") {
 			// console.log("start");
@@ -119,6 +136,12 @@ function numbersChoice (number) {
 }
 
 function actionChoice (action) {
+	numberNumber++;
+	if (actionsArray[actionNumber] == null) {
+		actionsArray[actionNumber] = "";
+	}
+	actionsArray[actionNumber] = action;
+	actionNumber++;
 	number1 = document.getElementById("numbers-box").innerHTML;
 	if (clearBoard == "start") {
 		RegularACChoice();
@@ -143,9 +166,14 @@ function actionChoice (action) {
 	number2Start = "start";
 
 	console.log(action);
-	if (action == "^") {
-		document.getElementById("power-numbers-box").style.border = "1px solid black";
-		document.getElementById("power-numbers-box").style.marginTop = "0";
+	if (thereWasPower == "yes" || action == "^") {
+		if (thereWasPower == "yes") {
+			document.getElementById("numbers-box2").innerHTML += action;
+		}
+		if (action == "^") {
+			document.getElementById("power-numbers-box").style.border = "1px solid black";
+			document.getElementById("power-numbers-box").style.marginTop = "0";
+		}
 	}
 	else {
 		document.getElementById("numbers-box").innerHTML += action;
@@ -153,7 +181,10 @@ function actionChoice (action) {
 }
 
 function checkChoice () {
+	// console.log("number 1: ", numbersArray[0], " number 2: ", numbersArray[1], "number 3: ", numbersArray[2]);
+	console.log(numbersArray[0] + actionsArray[0] + numbersArray[1] + actionsArray[1] + numbersArray[2]);
 	number2Start = "";
+	thereWasPower = "";
 	// if (number1 == "") {
 	// 	number1 = parseInt(result);
 	// }
@@ -172,9 +203,9 @@ function checkChoice () {
 
 			console.log(result + ", " + result2);
 			document.getElementById("quadratic-equation-result-X1").innerHTML = "X";
-			document.getElementById("quadratic-equation-result-1").innerHTML = "1";
 			document.getElementById("quadratic-equation-result1-numbers").innerHTML = "= " + result;
 			if (result !== result2) {
+				document.getElementById("quadratic-equation-result-1").innerHTML = "1";
 				document.getElementById("quadratic-equation-result-X2").innerHTML = "X";
 				document.getElementById("quadratic-equation-result-2").innerHTML = "2";
 				document.getElementById("quadratic-equation-result2-numbers").innerHTML = "= " + result2;
@@ -196,40 +227,215 @@ function checkChoice () {
 		}
 		console.log(Number(number1) + " " + globalAction + " " + Number(number2))
 
-		if (globalAction == "sin" || globalAction == "cos" || globalAction == "tan") {
-			if (globalDegOrRad == "deg") {
-				number2 = number2*Math.PI/180;
+		// if (globalAction == "sin" || globalAction == "cos" || globalAction == "tan") {
+		// 	if (globalDegOrRad == "deg") {
+		// 		number2 = number2*Math.PI/180;
+		// 	}
+		// }
+		// actionsOrder = 0;
+		// numbersOrder = 0;
+		var iArray = [];
+		var iArrayCheck = 0;
+		// var plusMinusCheck = 0;
+		// var iArrayPlace = 0;
+
+		for (var i = 0; i < actionsArray.length; i++) {
+			if (actionsArray[i] == "^" || actionsArray[i] == "Sqrt" || actionsArray[i] == "&sqrt;") {
+				actionsOrderArray[actionsOrderArray.length] = actionsArray[i];
+				iArrayCheck = 0;
+				for (var j = 0; j < iArray.length; j++) {
+					if (i !== iArray[j]) {
+						iArrayCheck++;
+					}
+				}
+				if (iArrayCheck == iArray.length) {
+					numbersOrderArray[numbersOrderArray.length] = numbersArray[i];
+					iArray[iArray.length] = i;
+				}
+				iArrayCheck = 0;
+				for (var j = 0; j < iArray.length; j++) {
+					if ((i+1) !== iArray[j]) {
+						iArrayCheck++;
+					}
+				}
+				if (iArrayCheck == iArray.length) {
+					numbersOrderArray[numbersOrderArray.length] = numbersArray[i+1];
+					iArray[iArray.length] = i+1;
+				}
+				// actionsOrder++;
+			}
+		}
+		for (var i = 0; i < actionsArray.length; i++) {
+			if (actionsArray[i] == "X" || actionsArray[i] == "/") {
+				actionsOrderArray[actionsOrderArray.length] = actionsArray[i];
+				iArrayCheck = 0;
+				for (var j = 0; j < iArray.length; j++) {
+					if (i !== iArray[j]) {
+						iArrayCheck++;
+					}
+				}
+				if (iArrayCheck == iArray.length) {
+					numbersOrderArray[numbersOrderArray.length] = numbersArray[i];
+					iArray[iArray.length] = i;
+					console.log("X, i");
+					console.log("i: " + i);
+					console.log("numbersArray[i]: " + numbersArray[i]);
+					// console.log("actionsOrder: " + actionsOrder);
+					// console.log("numbersOrderArray[actionsOrder]: " + numbersOrderArray[actionsOrder]);
+					console.log("numbersOrderArray: " + numbersOrderArray);
+				}
+				iArrayCheck = 0;
+				for (var j = 0; j < iArray.length; j++) {
+					if ((i+1) !== iArray[j]) {
+						iArrayCheck++;
+					}
+				}
+				if (iArrayCheck == iArray.length) {
+					numbersOrderArray[numbersOrderArray.length] = numbersArray[i+1];
+					iArray[iArray.length] = i+1;
+					console.log("X, (i+1)");
+					console.log("i: " + i);
+					console.log("numbersArray[i+1]: " + numbersArray[i+1]);
+					// console.log("actionsOrder: " + actionsOrder);
+					// console.log("numbersOrderArray[actionsOrder+1]: " + numbersOrderArray[actionsOrder+1]);
+					console.log("numbersOrderArray: " + numbersOrderArray);
+				}
+				// actionsOrder++;
+			}
+		}
+		for (var i = 0; i < actionsArray.length; i++) {
+			if (actionsArray[i] == "+" || actionsArray[i] == "-") {
+				actionsOrderArray[actionsOrderArray.length] = actionsArray[i];
+				iArrayCheck = 0;
+				for (var j = 0; j < iArray.length; j++) {
+					if (i !== iArray[j]) {
+						iArrayCheck++;
+					}
+				}
+				if (iArrayCheck == iArray.length) {
+					numbersOrderArray[numbersOrderArray.length] = numbersArray[i];
+					iArray[iArray.length] = i;
+					// if (actionsArray[i] == "-") {
+					// 	plusMinusCheck++;
+					// }
+					console.log("-, i");
+					console.log("iArray: " + iArray);
+					console.log("i: " + i);
+					console.log("numbersArray[i]: " + numbersArray[i]);
+					// console.log("actionsOrder: " + actionsOrder);
+					// console.log("numbersOrderArray[actionsOrder]: " + numbersOrderArray[actionsOrder]);
+					console.log("numbersOrderArray: " + numbersOrderArray);
+				}
+				iArrayCheck = 0;
+				for (var j = 0; j < iArray.length; j++) {
+					if ((i+1) !== iArray[j]) {
+						iArrayCheck++;
+					}
+				}
+				if (iArrayCheck == iArray.length) {
+					numbersOrderArray[numbersOrderArray.length] = numbersArray[i+1];
+					iArray[iArray.length] = i+1;
+					console.log("-, (i+1)");
+					console.log("i: " + i);
+					console.log("numbersArray[i+1]: " + numbersArray[i+1]);
+					// console.log("actionsOrder: " + actionsOrder);
+					// console.log("numbersOrderArray[actionsOrder+1]: " + numbersOrderArray[actionsOrder+1]);
+					console.log("numbersOrderArray: " + numbersOrderArray);
+				}
+				// actionsOrder++;
 			}
 		}
 
-		if (globalAction == "+") {
-			result = Number(number1) + Number(number2);
+		actionsArray = actionsOrderArray;
+		numbersArray = numbersOrderArray;
+		console.log(numbersArray);
+		console.log(actionsArray);
+
+		var temporalResult = Number(numbersArray[0]);
+
+		for (var i = 0; i < actionsArray.length; i++) {
+
+			if (actionsArray[i] == "sin" || actionsArray[i] == "cos" || actionsArray[i] == "tan") {
+				if (globalDegOrRad == "deg") {
+					number2 = number2*Math.PI/180;
+				}
+			}
+
+			// if ((plusMinusCheck % 2) == 1) {
+			// 	if (actionsArray[i] == "+") {
+			// 		actionsArray[i] = "-";
+			// 	}
+			// 	if (actionsArray[i] =="-") {
+			// 		actionsArray[i] = "+";
+			// 	}
+			// }
+
+			if (actionsArray[i] == "+") {
+				result = temporalResult + Number(numbersArray[i+1]);
+			}
+			if (actionsArray[i] == "-") {
+				result = temporalResult - Number(numbersArray[i+1]);
+			}
+			if (actionsArray[i] == "X") {
+				result = temporalResult * Number(numbersArray[i+1]);
+			}
+			if (actionsArray[i] == "/") {
+				result = temporalResult / Number(numbersArray[i+1]);
+			}
+			if (actionsArray[i] == "^") {
+				result = Math.pow(temporalResult, Number(numbersArray[i+1]));
+			}
+			if (actionsArray[i] == "Sqrt") {
+				result = Math.sqrt(Number(numbersArray[i+1]));
+			}
+			if (actionsArray[i] == "sin") {
+				result = Math.sin(Number(numbersArray[i+1]));
+			}
+			if (actionsArray[i] == "cos") {
+				result = Math.cos(Number(numbersArray[i+1]));
+			}
+			if (actionsArray[i] == "tan") {
+				result = (Math.sin(Number(numbersArray[i+1])))/(Math.cos(Number(numbersArray[i+1])));
+			}
+
+			temporalResult = result;
+
 		}
-		if (globalAction == "-") {
-			result = Number(number1) - Number(number2);
-		}
-		if (globalAction == "X") {
-			result = Number(number1) * Number(number2);
-		}
-		if (globalAction == "/") {
-			result = Number(number1) / Number(number2);
-		}
-		if (globalAction == "^") {
-			result = Math.pow(Number(number1), Number(number2));
-		}
-		if (globalAction == "&Sqrt;") {
-			result = Math.sqrt(Number(number2));
-		}
-		if (globalAction == "sin") {
-			result = Math.sin(Number(number2));
-		}
-		if (globalAction == "cos") {
-			result = Math.cos(Number(number2));
-		}
-		if (globalAction == "tan") {
-			result = (Math.sin(Number(number2)))/(Math.cos(Number(number2)));
-		}
+
+		// if (globalAction == "+") {
+		// 	result = Number(number1) + Number(number2);
+		// }
+		// if (globalAction == "-") {
+		// 	result = Number(number1) - Number(number2);
+		// }
+		// if (globalAction == "X") {
+		// 	result = Number(number1) * Number(number2);
+		// }
+		// if (globalAction == "/") {
+		// 	result = Number(number1) / Number(number2);
+		// }
+		// if (globalAction == "^") {
+		// 	result = Math.pow(Number(number1), Number(number2));
+		// }
+		// if (globalAction == "&Sqrt;") {
+		// 	result = Math.sqrt(Number(number2));
+		// }
+		// if (globalAction == "sin") {
+		// 	result = Math.sin(Number(number2));
+		// }
+		// if (globalAction == "cos") {
+		// 	result = Math.cos(Number(number2));
+		// }
+		// if (globalAction == "tan") {
+		// 	result = (Math.sin(Number(number2)))/(Math.cos(Number(number2)));
+		// }
 		console.log(result);
+
+		// if ((plusMinusCheck % 2) == 1) {
+		// 		result = -result;
+		// }
+
+		// console.log(result);
 		// var resultToShow = Math.round(result*Math.pow(10, 10))/Math.pow(10, 10);
 		// document.getElementById("numbers-box").innerHTML += "<br>= " + resultToShow;
 		result = Math.round(result*Math.pow(10, 10))/Math.pow(10, 10);
@@ -260,10 +466,19 @@ function ACChoice () {
 function RegularACChoice () {
 	document.getElementById("numbers-box").innerHTML = "0";
 	document.getElementById("power-numbers-box").innerHTML = "";
+	document.getElementById("numbers-box2").innerHTML = "";
 	document.getElementById("result-box").innerHTML = "";
-	number1 = "0";
-	number2 = "0";
+	// number1 = "0";
+	// number2 = "0";
+	// globalAction = "";
+	numbersArray = [];
+	actionsArray = [];
 	globalAction = "";
+	numbersOrderArray = [];
+	actionsOrderArray = [];
+	numberNumber = 0;
+	actionNumber = 0;
+	thereWasPower = "";
 }
 
 function QuadraticEquationACChoice () {
@@ -272,10 +487,12 @@ function QuadraticEquationACChoice () {
 		i++;
 	}
 	abc = 0;
+	//clear numbers
 	document.getElementById("quadratic-equation0").innerHTML = "";
 	document.getElementById("quadratic-equation1").innerHTML = "";
 	document.getElementById("quadratic-equation2").innerHTML = "";
 
+	//clear results
 	document.getElementById("quadratic-equation-result-X1").innerHTML = "";
 	document.getElementById("quadratic-equation-result-1").innerHTML = "";
 	document.getElementById("quadratic-equation-result1-numbers").innerHTML = "";
@@ -283,7 +500,10 @@ function QuadraticEquationACChoice () {
 	document.getElementById("quadratic-equation-result-2").innerHTML = "";
 	document.getElementById("quadratic-equation-result2-numbers").innerHTML = "";
 
+	//clear shadows
 	document.getElementById("quadratic-equation0").style.boxShadow = "0 0 10px #000";
+	document.getElementById("quadratic-equation1").style.boxShadow = "none";
+	document.getElementById("quadratic-equation2").style.boxShadow = "none";
 }
 
 function DegOrRadFunction (DegOrRad) {

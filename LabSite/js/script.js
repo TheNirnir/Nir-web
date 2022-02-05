7927cb42a075504940f768c7c95bb0015a2feb70
TheNirnir <nirnirba@gmail.com>
2022-02-05 11:15:31 +0200

    m

--- LabSite/js/script.js
+++ LabSite/js/script.js
@@ -795,6 +795,10 @@ function touchswipeStart () {
 	});
 }
 
+function goToFacebookPage() {
+	window.open("https://www.facebook.com/Positive-Family-Lab-Bar-Ilan-%D7%94%D7%9E%D7%A2%D7%91%D7%93%D7%94-%D7%9C%D7%97%D7%A7%D7%A8-%D7%94%D7%9E%D7%A9%D7%A4%D7%97%D7%94-%D7%91%D7%92%D7%99%D7%A9%D7%AA-%D7%94%D7%A4%D7%A1%D7%99%D7%9B%D7%95%D7%9C%D7%95%D7%92%D7%99%D7%94-%D7%94%D7%97%D7%99%D7%95%D7%91%D7%99%D7%AA-102172228828888", "_blank");	
+}
+
 //////////////////Accessabilty///////////////////
 var fontSize = 1;
 var gPageName = "Home";
